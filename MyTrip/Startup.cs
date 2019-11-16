acaca9d2a9f222b9e38592cc18f19517c8544943
bthompsonevan <bthompson.evan@gmail.com>
2019-11-16 10:04:50 -0800

    Setup the MVC environment from blank project

--- MyTrip/Startup.cs
+++ MyTrip/Startup.cs
@@ -15,20 +15,15 @@ namespace MyTrip
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Added MVC services to empty project
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //deleted Hello World code and added this code
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
