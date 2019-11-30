68dc0f643eaee308e9e3ac36179d90127ac4eef5
bthompso11 <bthompso11@gmail.com>
2019-11-29 22:01:35 -0800

    added AppDbContext class

---
+++ MyTrip/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyTrip.Models
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(
+            DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<TripAttendee> TripAttendees { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
