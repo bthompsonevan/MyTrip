b20fac710faac21ffd79d029156bc5d68d92ff15
bthompsonevan <bthompson.evan@gmail.com>
2019-12-07 12:30:45 -0800

    Added repository classes and methods to TripRepository.cs

---
+++ MyTrip/Repositories/TripRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyTrip.Models;
+
+namespace MyTrip.Repositories
+{
+    public class TripRepository
+    {
+        private AppDbContext context;
+
+        public List<Trip> Trips { get { return context.Trips.ToList(); } }
+       
+        public TripRepository(AppDbContext appDbContext)
+        {
+            context = appDbContext;
+        }
+
+        public void AddTrip(Trip trip)
+        {
+            context.Trips.Add(trip);
+            context.SaveChanges();
+        }
+
+        public void AddAttendee(Trip trip, TripAttendee tripAttendee)
+        {
+            trip.TripAttendees.Add(tripAttendee);
+            context.Trips.Update(trip);
+            context.SaveChanges();
+        }
+    }
+}
