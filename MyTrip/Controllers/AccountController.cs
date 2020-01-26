111a102d5302d9e36c7a456c93cf5ab6188a0967
bthompsonevan <bthompson.evan@gmail.com>
2020-01-25 19:15:00 -0800

    Added post login method to AccountController

--- MyTrip/Controllers/AccountController.cs
+++ MyTrip/Controllers/AccountController.cs
@@ -3,16 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MyTrip.Models;
 
 namespace MyTrip.Controllers
 {
     public class AccountController : Controller
     {
+
+        private UserManager<AppUser> userManager;
+        private SignInManager<AppUser> signInManager;
+        public AccountController(UserManager<AppUser> userMgr,
+              SignInManager<AppUser> signinMgr)
+        {
+            userManager = userMgr;
+            signInManager = signinMgr;
+        }
+
+
         [AllowAnonymous]
         public IActionResult Login(string returnUrl)
         {
             ViewBag.returnUrl = returnUrl; return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel details,
+                 string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await userManager.FindByEmailAsync(details.Email);
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result =
+                            await signInManager.PasswordSignInAsync(
+                                user, details.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+                ModelState.AddModelError(nameof(LoginViewModel.Email),
+                    "Invalid user or password");
+            }
+            return View(details);
         }
     }
 }
